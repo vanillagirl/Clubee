a55d1451172be0e0a0677281180d5b04638cff6d
Ingrid Funes <churro95@gmail.com>
2018-11-07 03:19:10 -0600

    Clubee - CSS update

--- Clubee/views.py
+++ Clubee/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Club
 
-# Create your views here.
+def principal(request):
+    clubes = Club.objects.all()
+    return render(request, 'Clubee/principal.html', {})
